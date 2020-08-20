08dd6b8988cfd1a3fe1dd120ea81d2c85e48845d
hajravasas <peteolah@gmail.com>
2020-08-20 15:18:48 -0500

    SearchParams broken out

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Pet from "./Pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
-  console.log("state of location", location);
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedString = apiBreeds.map(({ name }) => name);
+      setBreeds(breedString);
+    }, console.error);
+  }, [setBreed, setBreeds, animal]);
 
   return (
     <div className="search-params">
