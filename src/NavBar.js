c0e7199c90fa3131abd1b0b2b1002ac032975741
Peter Olah <peter.olah@care.com>
2020-09-15 10:15:45 -0500

    added inline css styling

---
+++ src/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "@reach/router";
+import { css, keyframes } from "@emotion/core";
+import colors from "./color";
+
+const Spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const NavBar = () => (
+  <header
+    css={css`
+      background-color: ${colors.dark};
+      position: sticky;
+      top: 0;
+      z-index: 10;
+    `}
+  >
+    <Link
+      css={css`
+        &:hover {
+          text-decoration: underline;
+        }
+      `}
+      to="/"
+    >
+      Adopt Me!
+    </Link>
+    <span
+      css={css`
+        display: inline-block;
+        animation: 1s ${Spin} linear infinite;
+        font-size: 60px;
+      `}
+      aria-label="logo"
+      role="img"
+    >
+      🐩
+    </span>
+  </header>
+);
+
+export default NavBar;
